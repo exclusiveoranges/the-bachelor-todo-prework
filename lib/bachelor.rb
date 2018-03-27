5e505b2d35105795870e3fd9c50c3d8f7816c130
Ryan O'Toole <otoolerj@gmail.com>
2018-03-27 00:52:18 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,6 @@ def get_first_name_of_season_winner(data, season)
   new_array = []
   contestant_array = data[season]
   contestant_array.collect do |hash|
-
     hash.collect do |key, value|
       if value == "Winner"
         season_winner = hash["name"]
