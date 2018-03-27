f9d3ea428117aada7dd7984081d363e97a86c4ed
Ryan O'Toole <otoolerj@gmail.com>
2018-03-27 00:36:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,7 +6,7 @@ def get_first_name_of_season_winner(data, season)
     # binding.pry
     hash.each do |key, value|
       if value = "Winner"
-      return key
+        hash["name"]
       end
     end
   end
