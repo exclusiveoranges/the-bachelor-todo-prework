d75a1f4b05d230b329646e67cf1537e3a6a045ed
Ryan O'Toole <otoolerj@gmail.com>
2018-03-27 01:52:27 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -24,9 +24,8 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # counter = 0
+  counter = 0
   data.keys.each do |season|
-    counter = 0
     data[season].each do |hash|
       hash.each do |key, value|
         if value == hometown
